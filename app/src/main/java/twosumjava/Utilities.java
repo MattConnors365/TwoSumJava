3d39dfd9f75ebdadbed6bd940cca4e32081c6ef5
Matt Connors <matt.connorsx@gmail.com>
2025-07-17 10:50:05 +0300

    first commit

---
+++ app/src/main/java/twosumjava/Utilities.java
@@ -0,0 +1,17 @@
+package twosumjava;
+
+import java.util.Scanner;
+
+public class Utilities {
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static int getInput(String prompt) {
+      System.out.print(prompt);
+      while (!scanner.hasNextInt()) {
+          System.out.println("Invalid input. Please enter an integer.");
+          scanner.next(); // Clear the invalid input
+          System.out.print(prompt);
+      }
+      return scanner.nextInt();
+  }
+}
